02a358890b2cc2ab91aed538784912fbc823430e
Daniel-Hebenstreit <111197814+Daniel-Hebenstreit@users.noreply.github.com>
2022-08-14 23:57:50 +0200

    Update main.cc

--- main.cc
+++ main.cc
@@ -3,10 +3,6 @@
 
 int main()
 {
-
-    // Eingabewerte. Dritter Wert ist jeweils korrekter bzw. erwarter Wert
-    //double matrix[4][3] = {{2, 5, 1}, {5, 4, 0}, {3, 3, 0}, {3, 6, 1}};
-
     update_weights();
 
     return 0;
